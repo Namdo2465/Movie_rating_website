68e5c29423baacb7d1e3642886b5093221df404d
Nam Đỗ <namdo@Nams-MacBook-Air.local>
2026-03-10 15:13:44 -0700

    Full movie-rating web app

---
+++ main.py
@@ -0,0 +1,121 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
+
+# Create DB
+class Base(DeclarativeBase):
+  pass
+
+# Configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+
+# Create movie table
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+
+    year: Mapped[int] = mapped_column(Integer, nullable = True)
+    description: Mapped[str] = mapped_column(String(250), nullable = True)
+    rating: Mapped[float] = mapped_column(Float, nullable = True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable = True)
+    review: Mapped[str] = mapped_column(String(250), nullable = True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable = True)
+
+# Create table schema in the database
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def home():
+    # READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating.desc()))
+    # Use .scalars() to get the elements
+    all_movies = result.scalars().all()
+    for index, movie in enumerate(all_movies):
+        movie.ranking = index + 1
+    db.session.commit()
+    return render_template("index.html", movies=all_movies)
+
+# Rate movie form
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5", validators=[DataRequired()])
+    review = StringField("Your Review", validators=[DataRequired()])
+    submit = SubmitField("Done")
+
+@app.route("/edit", methods = ["GET", "POST"])
+def rate_movie():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    form = RateMovieForm(rating=movie.rating, review=movie.review)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
+
+@app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+# Add movie form
+class AddMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_API_KEY = os.getenv("TMDB_API_KEY")
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = AddMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json().get("results", [])
+        return render_template("select.html", options=data)
+    return render_template("add.html", form = form)
+
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            #The data in release_date includes month and day, we will want to get rid of.
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("rate_movie", id=new_movie.id))
+print("API KEY:", MOVIE_DB_API_KEY)
+if __name__ == '__main__':
+    app.run(debug=True)
