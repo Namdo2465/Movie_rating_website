88320847b859cfca2ef25027e665583aa9930b78
Nam Đỗ <namdo@Nams-MacBook-Air.local>
2026-03-11 11:23:21 -0700

    Update environment variables

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import requests
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("APP_SECRET_KEY")
 
 
 # Create DB
